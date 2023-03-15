7afced3853052728de9008ed11a590f532c3612f
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-03-15 11:25:37 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ 
   #管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
@@ -8,5 +9,22 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :recipes, only: [:show, :edit, :update, :destroy]
+    resources :comments, only: [:index, :show, :update, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :sessions, only: [:new, :create, :destroy]
+  end
+  
+  scope module: :public do
+    root to: "homes#top"
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :users, only: [:show, :edit, :update]
+    get 'users/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+    patch 'users/withdraw' => 'customers#withdraw', as: 'withdraw'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
