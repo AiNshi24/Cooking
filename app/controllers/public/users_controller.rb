1cdeb2ba3c61422d4310b173b762c898dac0f668
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-03-16 13:19:19 +0000

    [Update]users_controller

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,14 +1,27 @@
 class Public::UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update, :withdraw]
+  
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "編集を保存しました"
+      redirect_to user_path(@user.id)
+    else render :edit
+    end 
+  end 
 
   def unsubscribe
   end
   
-  def withdrawal
+  def withdraw
     @user = User.find(params[:id])
     # is_deletedカラムをtrueに変更することにより削除フラグを立てる
     @user.update(is_deleted: true)
@@ -16,4 +29,19 @@ class Public::UsersController < ApplicationController
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :introduction, :profile_image, :is_deleted)
+    end
+    
+    #アクセス制限
+    def is_matching_login_user
+      user_id = params[:id].to_i
+      unless user_id == current_user.id
+        redirect_to user_path(current_user.id)
+      end
+    end
+    
 end
