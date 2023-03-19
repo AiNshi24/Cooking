858260163e235751380c6669ab1ae61c952474e4
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-03-19 06:27:13 +0000

    [Update]guest_login

--- app/controllers/public/guests_controller.rb
+++ app/controllers/public/guests_controller.rb
@@ -1,5 +1,5 @@
 class Public::GuestsController < ApplicationController
-  
+
   def guest_sign_in
     user = User.find_or_create_by!(name: "ゲスト", email: "guest@example.com", is_deleted: false) do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -8,5 +8,5 @@ class Public::GuestsController < ApplicationController
     sign_in user
     redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
   end
-  
+
 end
