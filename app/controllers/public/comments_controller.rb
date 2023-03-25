59c2fb0a7cbbf883a55c5d9a68577fe065707064
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-03-25 14:28:34 +0000

    [Update]レシピ詳細画面

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,6 +2,12 @@ class Public::CommentsController < ApplicationController
   def index
   end
   
+  def new
+    recipe = Recipe.find(params[:id])
+    comment = current_user.comments.new(comment_params)
+    comment.recipe_id = recipe.id
+  end
+  
   def create
     recipe = Recipe.find(params[:id])
     comment = current_user.comments.new(comment_params)
